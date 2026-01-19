2007e6241ad9749b4002342acc0514a7bdfa7e6b
AnishPowar01 <powaranish74@gmail.com>
2026-01-19 10:08:53 +0530

    Added boiler plate for controller

--- src/main/java/com/example/uber/controller/BookingController.java
+++ src/main/java/com/example/uber/controller/BookingController.java
@@ -1,11 +1,26 @@
 package com.example.uber.controller;
 
+import com.example.uber.dto.BookingResponse;
+import com.example.uber.service.BookingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookings")
 @RequiredArgsConstructor
 public class BookingController {
+
+    private final BookingService bookingService;
+
+    @GetMapping
+    public ResponseEntity<List<BookingResponse>> getAllBookings()
+    {
+        return ResponseEntity.ok(bookingService.findAll());
+    }
+
 }
